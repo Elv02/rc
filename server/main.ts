3c3793121cc3d5966aba4f1a556e7962d3106c9c
Elv02 <elv02x@gmail.com>
2024-07-09 20:10:54 -0400

    Moved texture assets to be hosted as part of the backend service

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,13 @@
 import { ConfigIniParser } from "config-ini-parser";
 import { readFile } from "fs";
-
+import express from "express";
+import http from "http";
+import path from "path";
+import { fileURLToPath } from "url";
 import { GameSocketServer } from "./src/gameSocketServer";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function myMain() {
   console.log("Launching RC Server under the following configuration:");
@@ -12,8 +18,21 @@ function myMain() {
     }
     parser.parse(data);
     console.log(parser.stringify());
-    const IP = parser.get("Connection", "IP");
-    const PORT = parser.get("Connection", "PORT");
+    
+    const IP = parser.get("Connections", "IP");
+    const PORT = parser.get("Connections", "PORT");
+    const ASSETS_IP = parser.get("Connections", "ASSETS_IP");
+    const ASSETS_PORT = parser.get("Connections", "ASSETS_PORT");
+
+    // Create an Express app
+    const app = express();
+    app.use(express.static(path.join(__dirname, 'asset_server')));
+
+    // Create an HTTP server
+    const httpServer = http.createServer(app);
+    httpServer.listen(ASSETS_PORT, ASSETS_IP, () => {
+      console.log(`HTTP server running at http://${ASSETS_IP}:${ASSETS_PORT}`);
+    });
 
     const GSS = new GameSocketServer({ "host" : URL, "port": PORT });
     GSS.start();
